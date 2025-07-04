c986a7a94439e68aa9e556404ed5563aba531e20
venkat naidu <venkatadrichinnu@gmail.com>
2025-07-04 11:56:57 +0530

    ETL pipeline completed

--- test_mongodb.py
+++ test_mongodb.py
@@ -1,7 +1,7 @@
 
 from pymongo.mongo_client import MongoClient
 
-uri = "mongodb+srv://krishaitechnologies:<@password>@cluster0.wnqb6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = "mongodb+srv://networksecurity:naidu123@cluster0.h4evhfx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Create a new client and connect to the server
 client = MongoClient(uri)
