994ae23dc22e889d5b57183aa6dc3ee3e9863e04
venkat naidu <venkatadrichinnu@gmail.com>
2025-07-07 19:30:10 +0530

    Deployment changes

--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -9,6 +9,7 @@ from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.components.model_trainer import ModelTrainer
 
+
 from networksecurity.entity.config_entity import(
     TrainingPipelineConfig,
     DataIngestionConfig,
